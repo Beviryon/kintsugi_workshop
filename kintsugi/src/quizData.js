90e3d9bbace9b83d3736fda6f1ff0d8755e7ce29
Beviryon ISSANGA-NGOULOU <beviryon.issanga-ngoulou@eurexo-ced.fr>
2024-06-28 12:47:49 +0200

    chatbot

---
+++ kintsugi/src/quizData.js
@@ -0,0 +1,94 @@
+// quizData.js
+
+export const quizData = [
+  {
+    question: "Comment vous sentez-vous aujourd'hui ?",
+    options: ["Bien", "Pas très bien", "Très mal"],
+    responses: [
+      "C'est super ! Continuez à prendre soin de vous.",
+      "Je suis désolé d'entendre ça. Pouvez-vous me décrire vos symptômes ?",
+      "Je suis vraiment désolé que vous vous sentiez si mal. Voulez-vous que je vous mette en contact avec un professionnel de santé ?"
+    ]
+  },
+  {
+    question: "Avez-vous de la fièvre ?",
+    options: ["Oui", "Non", "Je ne suis pas sûr"],
+    responses: [
+      "La fièvre peut être un signe d'infection. Depuis combien de temps avez-vous de la fièvre ?",
+      "C'est bon signe. Avez-vous d'autres symptômes ?",
+      "Il serait utile de prendre votre température. Avez-vous un thermomètre à la maison ?"
+    ]
+  },
+  {
+    question: "Ressentez-vous de l'anxiété ou du stress ?",
+    options: ["Oui, beaucoup", "Un peu", "Non, pas vraiment"],
+    responses: [
+      "Je comprends que cette situation puisse être stressante. Voulez-vous que je vous donne quelques techniques de relaxation ?",
+      "C'est normal de ressentir un peu d'anxiété. Essayez de prendre de profondes respirations et de vous concentrer sur des pensées positives.",
+      "C'est bien que vous vous sentiez calme. Continuez à prendre soin de votre santé mentale autant que de votre santé physique."
+    ]
+  },
+  {
+    question: "Avez-vous des difficultés à respirer ?",
+    options: ["Oui", "Un peu", "Non"],
+    responses: [
+      "Des difficultés respiratoires peuvent être sérieuses. Je vous recommande de consulter un médecin rapidement.",
+      "Surveillez attentivement ce symptôme. Si cela s'aggrave, n'hésitez pas à consulter un médecin.",
+      "C'est rassurant. Continuez à surveiller vos symptômes et n'hésitez pas à me tenir informé de tout changement."
+    ]
+  },
+  {
+    question: "Avez-vous perdu le goût ou l'odorat récemment ?",
+    options: ["Oui", "Non", "Je ne suis pas sûr"],
+    responses: [
+      "La perte du goût ou de l'odorat peut être un symptôme de certaines infections. Il serait prudent de vous isoler et de consulter un médecin.",
+      "C'est un bon signe. Continuez à surveiller vos symptômes.",
+      "Essayez de faire un test simple, comme sentir du café ou goûter quelque chose de fort. Si vous avez des doutes, il vaut mieux consulter."
+    ]
+  },
+  {
+    question: "Avez-vous été en contact avec quelqu'un qui a été testé positif au COVID-19 ?",
+    options: ["Oui", "Non", "Je ne suis pas sûr"],
+    responses: [
+      "Il est recommandé de vous isoler et de vous faire tester. Voulez-vous que je vous donne des informations sur les centres de test près de chez vous ?",
+      "C'est rassurant. Continuez néanmoins à suivre les mesures de précaution recommandées.",
+      "Par précaution, il serait sage de limiter vos contacts et de surveiller l'apparition de symptômes. N'hésitez pas à vous faire tester au moindre doute."
+    ]
+  },
+  {
+    question: "Comment est votre sommeil ces derniers temps ?",
+    options: ["Bon", "Perturbé", "Très mauvais"],
+    responses: [
+      "Un bon sommeil est essentiel pour la santé. Continuez à maintenir une bonne hygiène de sommeil.",
+      "Un sommeil perturbé peut affecter votre santé. Avez-vous essayé des techniques de relaxation avant de dormir ?",
+      "Un mauvais sommeil peut avoir un impact significatif sur votre santé. Voulez-vous que je vous donne quelques conseils pour améliorer votre sommeil ?"
+    ]
+  },
+  {
+    question: "Comment décririez-vous votre niveau d'énergie ?",
+    options: ["Normal", "Fatigué", "Épuisé"],
+    responses: [
+      "C'est bien que vous vous sentiez en forme. Continuez à prendre soin de vous.",
+      "La fatigue peut être due à de nombreux facteurs. Assurez-vous de bien vous reposer et de manger équilibré.",
+      "Un épuisement constant peut être le signe d'un problème de santé. Il serait peut-être bon d'en parler à un médecin."
+    ]
+  },
+  {
+    question: "Avez-vous des douleurs musculaires ou articulaires ?",
+    options: ["Oui", "Un peu", "Non"],
+    responses: [
+      "Des douleurs persistantes méritent attention. Avez-vous essayé des étirements doux ou des applications de chaleur ?",
+      "Des douleurs légères peuvent souvent être soulagées par du repos et des exercices doux. Si elles persistent, consultez un médecin.",
+      "C'est bien que vous n'ayez pas de douleurs. L'exercice régulier peut aider à maintenir cette condition."
+    ]
+  },
+  {
+    question: "Comment est votre appétit ces derniers jours ?",
+    options: ["Normal", "Diminué", "Augmenté"],
+    responses: [
+      "Un appétit normal est un bon signe. Continuez à maintenir une alimentation équilibrée.",
+      "Une perte d'appétit peut être due au stress ou à une maladie. Essayez de manger de petits repas fréquents et nutritifs.",
+      "Une augmentation de l'appétit peut être normale, mais si elle est accompagnée d'autres symptômes, il serait bon d'en parler à un médecin."
+    ]
+  }
+];
